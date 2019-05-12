766bac475cf09364137444373dfe229d1f89e964
Remi Rousselet <darky12s@gmail.com>
2019-05-12 06:16:52 +0200

    initial proxyprovider implementation

--- lib/provider.dart
+++ lib/provider.dart
@@ -4,4 +4,5 @@ export 'src/adaptive_builder_widget.dart' show ValueBuilder;
 export 'src/consumer.dart';
 export 'src/listenable_provider.dart';
 export 'src/provider.dart';
+export 'src/proxy_provider.dart';
 export 'src/stream_provider.dart';
